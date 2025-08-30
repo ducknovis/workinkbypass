2ec80121b966de76acb134a3bdf7e21a7153d1a3
ducknovis <liminhomcc@gmail.com>
2025-08-30 10:38:18 +0700

    Create workinkbypass.user.js

---
+++ workinkbypass.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name WorkInk Bypass (Volcano Bypass)
+// @namespace http://tampermonkey.net/
+// @version 1.0
+// @description No Wait No Install Extension Automatic Bypass Support bypass Volcano
+// @author Ducknovis
+// @match *://work.ink/*
+// @match *://workink.net/*
+// @require https://raw.githubusercontent.com/ducknovis/workinkbypass/refs/heads/main/script.js
+// @icon https://tr.rbxcdn.com/180DAY-17a64dbcdfded233559a0fd9572d70a2/150/150/Decal/Webp/noFilter
+// @run-at document-end
+// @grant none
+// ==/UserScript==
